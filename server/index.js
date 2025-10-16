a01ef62d30c6603b002ef23e740f963d5ddecfa5
phatcharat <ppom3059@gmail.com>
2025-10-16 23:58:06 +0700

    test deploy on railway

--- server/index.js
+++ server/index.js
@@ -114,6 +114,10 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, 'client_build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client_build', 'index.html'));
+});
 app.use('/uploads/avatars', express.static(UPLOAD_DIR));
 
 // POST signup
