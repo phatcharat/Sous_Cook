6bee4726fd08eb3fcce599b27501def6d58f561f
phatcharat <ppom3059@gmail.com>
2025-10-17 23:12:08 +0700

    fix server dockerfile

---
+++ server/build/precache-manifest.2d2bf4b6b0df66c965884ff265d316c0.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933e59321001b91ceb95c9eb67c4480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d82fdd339143552719a",
+    "url": "/static/css/main.60e0432f.chunk.css"
+  },
+  {
+    "revision": "e3696512710d13b1cd40",
+    "url": "/static/js/2.9d8127f9.chunk.js"
+  },
+  {
+    "revision": "85bc138f08f79ab88191f4eb21e0ed09",
+    "url": "/static/js/2.9d8127f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d82fdd339143552719a",
+    "url": "/static/js/main.2284f8ff.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "92f877a141089cc95fff44164853a3fe",
+    "url": "/static/media/Add_btn.92f877a1.svg"
+  },
+  {
+    "revision": "d6595286df2fcb4035d7fb3fe7f7d450",
+    "url": "/static/media/Back.d6595286.svg"
+  },
+  {
+    "revision": "8f303be81d372b2d8d3f2224de61249b",
+    "url": "/static/media/Banner1.8f303be8.svg"
+  },
+  {
+    "revision": "e052e89e629fd12a706693b429b3682c",
+    "url": "/static/media/Banner2.e052e89e.svg"
+  },
+  {
+    "revision": "d07d928625f664f154f26d21227f4324",
+    "url": "/static/media/Banner3.d07d9286.svg"
+  },
+  {
+    "revision": "179575634cccc173c17bcc5d0f9aef82",
+    "url": "/static/media/Banner4.17957563.svg"
+  },
+  {
+    "revision": "31215a6cd4bc87f042a0eea84cc77d2a",
+    "url": "/static/media/Bread.31215a6c.svg"
+  },
+  {
+    "revision": "ba6cb344fe25ff21613acf21d152b3f1",
+    "url": "/static/media/BreadClick.ba6cb344.svg"
+  },
+  {
+    "revision": "dc6aeedca57ab6fa63a1059c6df2fbae",
+    "url": "/static/media/Celery.dc6aeedc.svg"
+  },
+  {
+    "revision": "cd91cfd29bdf94a6ce5b4be0d25ce7cb",
+    "url": "/static/media/CeleryClick.cd91cfd2.svg"
+  },
+  {
+    "revision": "00578017f7e446b93c69079e9d5da94a",
+    "url": "/static/media/Checkbox.00578017.svg"
+  },
+  {
+    "revision": "9af2840631421f94a94681e48404ef3d",
+    "url": "/static/media/Checkbox_check.9af28406.svg"
+  },
+  {
+    "revision": "3a0f54a28145f27ed133a8e6a5a30f1f",
+    "url": "/static/media/Egg, milk, and dairy product.3a0f54a2.svg"
+  },
+  {
+    "revision": "ced78b46c0553e1d29137f613e4ed0ff",
+    "url": "/static/media/Fats and oils.ced78b46.svg"
+  },
+  {
+    "revision": "a4fda5e97e36983206d56e7c0e2776cf",
+    "url": "/static/media/Favorite.a4fda5e9.svg"
+  },
+  {
+    "revision": "55d64a893a71abe2b9fd9f17d148860b",
+    "url": "/static/media/Fruits.55d64a89.svg"
+  },
+  {
+    "revision": "d13393925c3f52331eecf2ecd8ab6ad3",
+    "url": "/static/media/Grab.d1339392.svg"
+  },
+  {
+    "revision": "80396d64f6d22a3181d21d23acc6fd55",
+    "url": "/static/media/Grains, nuts, and baking products.80396d64.svg"
+  },
+  {
+    "revision": "787397f2a19174f745941d3f5d038a90",
+    "url": "/static/media/Header.787397f2.svg"
+  },
+  {
+    "revision": "d50bfc955aa4e67dbd5da3b4cb4245e5",
+    "url": "/static/media/Herbs and spices.d50bfc95.svg"
+  },
+  {
+    "revision": "57b497bea9ab8817e1de4a750025cb7d",
+    "url": "/static/media/Line_man.57b497be.png"
+  },
+  {
+    "revision": "474b1bd14e4b59550f46eafdf44ccb66",
+    "url": "/static/media/List.474b1bd1.svg"
+  },
+  {
+    "revision": "79899453d2be452eeb325fa72348c71a",
+    "url": "/static/media/Logo.79899453.svg"
+  },
+  {
+    "revision": "caea3fae01699ef520e36aae656589d6",
+    "url": "/static/media/Logo1.caea3fae.svg"
+  },
+  {
+    "revision": "9e1e351c3fb16aa6e58e649d9d2641d4",
+    "url": "/static/media/Meat, sausages and fish.9e1e351c.svg"
+  },
+  {
+    "revision": "36be35c4df4c0f66a73772f588f80555",
+    "url": "/static/media/News.36be35c4.svg"
+  },
+  {
+    "revision": "172cd7196fe12b2add448f62958f2c9a",
+    "url": "/static/media/Next_Button.172cd719.svg"
+  },
+  {
+    "revision": "7dc13f38f6b933e3246816834838f073",
+    "url": "/static/media/Other.7dc13f38.svg"
+  },
+  {
+    "revision": "0bdd218862c4a887ceba3afaa695c1b0",
+    "url": "/static/media/Pasta, rice, and pulses.0bdd2188.svg"
+  },
+  {
+    "revision": "82735d4be99b8c246f39840a1d4280a7",
+    "url": "/static/media/Pork.82735d4b.svg"
+  },
+  {
+    "revision": "e13d6f05c9ced7a80d98796ab65a17c8",
+    "url": "/static/media/PorkClick.e13d6f05.svg"
+  },
+  {
+    "revision": "56b3e6e381c872f4838c27a098978505",
+    "url": "/static/media/Random-menu.56b3e6e3.svg"
+  },
+  {
+    "revision": "bab926d4166bfe4da0e8133573e1531c",
+    "url": "/static/media/Retake_Button.bab926d4.svg"
+  },
+  {
+    "revision": "b16c504928ed8b590594916483743512",
+    "url": "/static/media/Search-Icon.b16c5049.svg"
+  },
+  {
+    "revision": "b48ca862c89519fd591590eb335f6aa3",
+    "url": "/static/media/TextLogo.b48ca862.svg"
+  },
+  {
+    "revision": "b5764a59d0c35d1af86fdec7fd96c7c5",
+    "url": "/static/media/Tomato.b5764a59.svg"
+  },
+  {
+    "revision": "7a4355c508a68065d67052c169935a76",
+    "url": "/static/media/TomatoClick.7a4355c5.svg"
+  },
+  {
+    "revision": "cca2d0fe1d76716755680340164a5116",
+    "url": "/static/media/Vegetable.cca2d0fe.svg"
+  },
+  {
+    "revision": "c71a47c569702132bab834b9ced6c92c",
+    "url": "/static/media/chef.c71a47c5.png"
+  },
+  {
+    "revision": "f2269fac2349b09eec72f6c0943f7dd8",
+    "url": "/static/media/close_btn.f2269fac.svg"
+  },
+  {
+    "revision": "a4fda5e97e36983206d56e7c0e2776cf",
+    "url": "/static/media/heart-filled.a4fda5e9.svg"
+  },
+  {
+    "revision": "bec7e939490c275d1fde716332a38e24",
+    "url": "/static/media/heart-outline.bec7e939.svg"
+  },
+  {
+    "revision": "98d90925e3d5e0ac20dd8f0110a796bc",
+    "url": "/static/media/icon_camera.98d90925.svg"
+  },
+  {
+    "revision": "c82c2993de3c689287fe226a6051e706",
+    "url": "/static/media/icon_history.c82c2993.svg"
+  },
+  {
+    "revision": "32ed2c0c7d131e883db8f51b4d68494d",
+    "url": "/static/media/icon_home.32ed2c0c.svg"
+  },
+  {
+    "revision": "0e5bb0e6feeda0630d312754ac459346",
+    "url": "/static/media/icon_like.0e5bb0e6.svg"
+  },
+  {
+    "revision": "a92335a32a91a534932f0a28cd516e3d",
+    "url": "/static/media/icon_people.a92335a3.svg"
+  },
+  {
+    "revision": "3a0cd2692cd5c6f2005aa0d232dd056b",
+    "url": "/static/media/notfound-image.3a0cd269.svg"
+  },
+  {
+    "revision": "c72d92468b904ddcfb9ae4cc7adffac2",
+    "url": "/static/media/picAboutMe2.c72d9246.jpg"
+  },
+  {
+    "revision": "7804ca5bdf5f4245e72bf3fb1e4e67a4",
+    "url": "/static/media/picAboutMe3.7804ca5b.svg"
+  },
+  {
+    "revision": "9616aff3c148c35dccefff93e29d9fc2",
+    "url": "/static/media/profile.9616aff3.jpg"
+  },
+  {
+    "revision": "5708043cb90cd68fba6840ddb6c42bce",
+    "url": "/static/media/takecamera_Default.5708043c.svg"
+  },
+  {
+    "revision": "d1888e61a3849cc6a53fa8170e466c24",
+    "url": "/static/media/takecamera_Onpress.d1888e61.svg"
+  },
+  {
+    "revision": "f922278015c132f60cb34c85ee995905",
+    "url": "/static/media/tips.f9222780.svg"
+  },
+  {
+    "revision": "ed22726c815a1592b80a07abe4fc6a89",
+    "url": "/static/media/unknow-ingredient.ed22726c.svg"
+  },
+  {
+    "revision": "a25673f8d04910c7b429a8bb955cfb76",
+    "url": "/static/media/upload.a25673f8.png"
+  }
+]);
